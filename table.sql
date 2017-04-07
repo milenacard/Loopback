74472a72830ac904c2fabb94c6f0a99237cd4a54
milena <mile.card195@gmail.com>
2017-04-07 17:06:37 -0500

    creacion de container

--- table.sql
+++ table.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 01-04-2017 a las 01:28:27
+-- Tiempo de generación: 04-04-2017 a las 15:36:23
 -- Versión del servidor: 5.5.24-log
 -- Versión de PHP: 5.4.3
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de datos: `databaseevent`
+-- Base de datos: `lab3`
 --
 
 -- --------------------------------------------------------
@@ -25,7 +25,6 @@ SET time_zone = "+00:00";
 --
 -- Estructura de tabla para la tabla `event`
 --
-USE lab3;
 
 CREATE TABLE IF NOT EXISTS `event` (
   `id` int(12) NOT NULL AUTO_INCREMENT,
@@ -46,9 +45,9 @@ CREATE TABLE IF NOT EXISTS `event` (
 --
 
 INSERT INTO `event` (`id`, `foto`, `nombre`, `descripcion`, `puntuacion`, `responsable`, `fecha`, `latitud`, `longitud`, `informacionGeneral`) VALUES
-(1, 'R.drawable.patinaje_artstico', 'Patinaje Artistico', 'Campeonato Mundial Junior 2017', '3', 'Taiwan', '15/03/2017', 25.04776, 121.53185, 'Este campeonato es el segundo en importancia tras los Juegos Olimpicos de Invierno y el Campeonato mundial de patinaje artístico tanto en tamanho como importancia.'),
+(1, 'R.drawable.campeonato_mundial_de_patinaje_artstico_junior_isu', 'Patinaje Artistico', 'Campeonato Mundial Junior 2017', '3', 'Taiwan', '15/03/2017', 25.04776, 121.53185, 'Este campeonato es el segundo en importancia tras los Juegos Olimpicos de Invierno y el Campeonato mundial de patinaje artístico tanto en tamanho como importancia.'),
 (2, 'R.drawable.tirodeportivo', 'Tiro Deportivo', 'Copa del Mundo de Shotgun', '4', 'Mexico', '19/03/2017', 16.86336, -99.8901, 'La ciudad de Acapulco será la sede de la Copa del Mundo de Shotgun 2017 de la ISSF'),
-(3, 'R.drawable.australia', 'Formula 1', 'Gran Premio de Australia', '3', 'Alemania', '26/03/2017', -27, 133, 'El Gran Premio de Australia de 2017 será la primera carrera de la temporada 2017 de Fórmula 1, que se disputara en el Circuito de Albert Park, en Melbourne (Australia).'),
+(3, 'R.drawable.australia_20122', 'Formula 1', 'Gran Premio de Australia', '3', 'Alemania', '26/03/2017', -27, 133, 'El Gran Premio de Australia de 2017 será la primera carrera de la temporada 2017 de Fórmula 1, que se disputara en el Circuito de Albert Park, en Melbourne (Australia).'),
 (4, 'R.drawable.hockey', 'Patinaje sobre hielo', 'Campeonato Mundial femenino 2017', '5', 'EEUU', '31/03/2017', 38, -97, 'Es la maxima competición internacional entre selecciones nacionales de hockey sobre hielo.'),
 (5, 'R.drawable.balonmano', 'Balonmano', 'XXV Campeonato Mundial de Balonmano Femenino', '2', 'Alemania', '01/12/2017', 51, 9, 'Un total de veinticuatro selecciones nacionales de cuatro confederaciones continentales competiran por el titulo de campeón mundial, cuyo actual portador es el equipo de Noruega, vencedor del Mundial de 2015.');
 
--- user.sql
+++ user.sql
@@ -3,13 +3,12 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 01-04-2017 a las 22:43:37
+-- Tiempo de generación: 04-04-2017 a las 16:54:01
 -- Versión del servidor: 5.5.24-log
 -- Versión de PHP: 5.4.3
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-USE lab3;
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -32,7 +31,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `username` varchar(10) NOT NULL,
   `password` varchar(10) NOT NULL,
   `nombre` varchar(40) NOT NULL,
-  `edad` int(3) NOT NULL,
+  `edad` varchar(3) NOT NULL,
   `email` varchar(20) NOT NULL,
   `foto` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -43,7 +42,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `nombre`, `edad`, `email`, `foto`) VALUES
-(1, 'Luisita', '1234567', 'Luisa Carvajal', 25, 'luisita123@gmail.com', NULL);
+(1, 'Luisita', '1234567', 'Luisa Carvajal', '25', 'luisita123@gmail.com', NULL);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
